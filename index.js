cee5b9fa6ccef092fe841f0c4fda3c250373531b
Marckenleydev <74468458+Marckenleydev@users.noreply.github.com>
2022-02-13 15:14:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const cors=require("cors");
+const mongoose=require("mongoose");
+const dotenv=require("dotenv");
+const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users")
+const postRoute = require("./routes/posts")
+const categoryRoute = require("./routes/categories")
+const multer = require("multer")
+const path = require("path")
+
+dotenv.config();
+app.use(cors())
+app.use(express.json())
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+app.use("/api/auth",authRoute)
+app.use("/api/users",userRoute)
+app.use("/api/posts",postRoute)
+app.use("/api/categories",categoryRoute)
+
+const storage = multer.diskStorage({
+    destination:(req, file,cb)=>{
+        cb(null, "images");
+    },
+    filename:(req,file, cb)=>{
+        cb(null, req.body.name)
+    }
+})
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+mongoose.connect(process.env.DATABASE_URL,{
+    // useNewUrlParser:true,
+    // useFindAndModify:true,
+    // usercreateIndex:true,
+    // useUnifiedTopology:true
+
+}, ()=>{
+    console.log("the connection to mongoDB  is successful")
+})
+app.listen("8000", ()=>{
+    console.log("Backend server is running");
+})
